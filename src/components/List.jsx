2d619768866178aec16d09bfc6c68cb61c06a10e
SamSherrill <38333695+SamSherrill@users.noreply.github.com>
2020-05-02 15:55:15 -0400

    deleted all references to profile pics, fixed my default formatter for vscode

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,22 +4,23 @@ import Item from "./Item";
 const List = (props) => {
   return (
     <div className="container">
-        <div className="row">
-            <h3 className="h3-employee-count">There are {props.employees.length} employees</h3>
-        </div>
-        <div className="row list-headers">
-            <div className="col-md-2">Profile Picture</div>
-            <div className="col-sm-1">ID #</div>
-            <div className="col-md-2">Full Name</div>
-            <div className="col-md-4">Email Address</div>
-            <div className="col-md-2">Phone Number</div>
-            <div className="col-sm-1">Age</div>
-        </div>
-        <div>
-            {props.employees.map((employee) => (
-              <Item {...employee} key={employee.id}/>
-            ))}
-        </div>
+      <div className="row">
+        <h3 className="h3-employee-count">
+          There are {props.employees.length} employees
+        </h3>
+      </div>
+      <div className="row list-headers">
+        <div className="col-sm-1">ID #</div>
+        <div className="col-md-3">Full Name</div>
+        <div className="col-md-5">Email Address</div>
+        <div className="col-md-2">Phone Number</div>
+        <div className="col-sm-1">Age</div>
+      </div>
+      <div>
+        {props.employees.map((employee) => (
+          <Item {...employee} key={employee.id} />
+        ))}
+      </div>
     </div>
   );
 };
